c665afb0466b4fb3edcfa98c1fd1d22404c77dbf
isabel <i.c.m.h@hotmail.com>
2026-06-01 12:17:37 -0500

    Proyecto MoveFit inicial

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent
+  ]
+})
+export class Tab1Page {}
