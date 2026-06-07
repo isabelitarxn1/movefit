f4c86f8e485b393ec0dd99b2f5e106526fc7c252
Faver Lopez <flopez@imagine-tech.com>
2026-06-07 14:31:02 -0500

    feat: Implementa nuevas páginas de configuración, perfil, progreso y rutinas; elimina componentes obsoletos

---
+++ src/app/pages/perfil/perfil.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['../home/section-placeholder.scss'],
+  standalone: true,
+  imports: [IonContent],
+})
+export class PerfilPage {}
