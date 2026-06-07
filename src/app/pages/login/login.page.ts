774b464ae9692093d3fab7102ee047bc4895ae6c
Faver Lopez <flopez@imagine-tech.com>
2026-06-07 14:24:55 -0500

    chore: Elimina código innecesario y simplifica la clase LoginPage

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { IonContent, IonIcon } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { arrowBackOutline } from 'ionicons/icons';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonIcon, RouterLink],
+  imports: [IonContent, RouterLink],
 })
-export class LoginPage {
-  constructor() {
-    addIcons({
-      arrowBackOutline,
-    });
-  }
-}
+export class LoginPage {}
